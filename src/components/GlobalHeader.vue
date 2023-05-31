d4bbe159b8e40c5147052b56ab767cdc76782c67
tnccc <irei.frontend@gmail.com>
2023-05-31 23:17:47 +0900

    first commit

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header :class="$style.header">
+    <div :class="$style.container">
+        header
+    </div>
+  </header>
+</template>
+
+<style lang="scss" module>
+.header {
+  width: 100%;
+}
+</style>
